b3d266a91c909ba6814a46550ed6e21197fcc82a
neytism <nateodnerolf@gmail.com>
2025-02-28 21:17:00 +0800

    - updated flyingLetters for mobile devices

--- assets/js/flyingLetters.js
+++ assets/js/flyingLetters.js
@@ -5,31 +5,19 @@ var grd,
     keys_down = [],
     letters = [];
 
-var symbols = [
-    { k: 81, s: "q" }, { k: 87, s: "w" }, { k: 69, s: "e" },
-    { k: 82, s: "r" }, { k: 84, s: "t" }, { k: 89, s: "y" },
-    { k: 85, s: "u" }, { k: 73, s: "i" }, { k: 79, s: "o" },
-    { k: 80, s: "p" }, { k: 65, s: "a" }, { k: 83, s: "s" },
-    { k: 68, s: "d" }, { k: 70, s: "f" }, { k: 71, s: "g" },
-    { k: 72, s: "h" }, { k: 74, s: "j" }, { k: 75, s: "k" },
-    { k: 76, s: "l" }, { k: 90, s: "z" }, { k: 88, s: "x" },
-    { k: 67, s: "c" }, { k: 86, s: "v" }, { k: 66, s: "b" },
-    { k: 78, s: "n" }, { k: 77, s: "m" }, { k: 48, s: "0" },
-    { k: 49, s: "1" }, { k: 50, s: "2" }, { k: 51, s: "3" },
-    { k: 52, s: "4" }, { k: 53, s: "5" }, { k: 54, s: "6" },
-    { k: 55, s: "7" }, { k: 56, s: "8" }, { k: 57, s: "9" }
-];
+const letterPool = [];
+const MAX_POOL_SIZE = 100;
 
-function Letter(key) {
-    // Generate a random starting x position within canvas width
+
+function Letter(char) {
     this.x = Math.floor(Math.random() * canvas.width);
-    
-    this.symbol = findS(key);
+    this.symbol = char;
     this.color = "rgba(255, 255, 255," + Math.random() + ")";
     this.size = Math.floor((Math.random() * 40) + 12);
     this.path = getRandomPath(this.x);
     this.rotate = Math.floor((Math.random() * Math.PI) + 1);
     this.percent = 0;
+    this.font = this.size + "px Arial";
 }
 
 Letter.prototype.draw = function() {
@@ -39,7 +27,7 @@ Letter.prototype.draw = function() {
     ctx.save();
     ctx.translate(xy.x, xy.y);
     ctx.rotate(this.rotate);
-    ctx.font = this.size + "px Arial";
+    ctx.font = this.font;
     ctx.fillStyle = this.color;
     ctx.fillText(this.symbol, -15, -15);
     ctx.restore();
@@ -53,14 +41,32 @@ Letter.prototype.drawPath = function() {
     ctx.stroke();
 }
 
-function findS(key) {
-    for (var i = 0; i < symbols.length; i++) {
-        if (symbols[i].k == key) {
-            return symbols[i].s;
-        }
+Letter.prototype.reset = function(char) {
+    this.x = Math.floor(Math.random() * canvas.width);
+    this.symbol = char;
+    this.color = "rgba(255, 255, 255," + Math.random() + ")";
+    this.size = Math.floor((Math.random() * 40) + 12);
+    this.font = this.size + "px Arial";
+    this.path = getRandomPath(this.x);
+    this.rotate = Math.floor((Math.random() * Math.PI) + 1);
+    this.percent = 0;
+};
+
+function getLetterFromPool(char) {
+    if (letterPool.length > 0) {
+        const letter = letterPool.pop();
+        letter.reset(char);
+        return letter;
     }
-    return false;
+    return new Letter(char);
 }
+
+function returnLetterToPool(letter) {
+    if (letterPool.length < MAX_POOL_SIZE) {
+        letterPool.push(letter);
+    }
+}
+
 
 function getRandomPath(x) {
     var x_start = x;
@@ -84,56 +90,58 @@ function drawBackground() {
 }
 
 function getQuadraticBezierXYatPercent(startPt, controlPt, endPt, percent) {
-	var x = Math.pow(1 - percent,2) * startPt.x 
-          + 2 * (1 - percent) * percent * controlPt.x 
-          + Math.pow(percent ,2) * endPt.x; 
+    var invPercent = 1 - percent;
+    var invPercentSq = invPercent * invPercent;
+    var percentSq = percent * percent;
     
-	var y = Math.pow(1 - percent ,2) * startPt.y 
-          + 2 * (1 - percent) * percent * controlPt.y 
-          + Math.pow(percent ,2) * endPt.y; 
+    var x = invPercentSq * startPt.x + 
+            2 * invPercent * percent * controlPt.x + 
+            percentSq * endPt.x;
     
-	return( {x:x,y:y} );
+    var y = invPercentSq * startPt.y + 
+            2 * invPercent * percent * controlPt.y + 
+            percentSq * endPt.y;
+    
+    return {x: x, y: y};
 }
 
 function resize() {
-	var box = canvas.getBoundingClientRect();
-	canvas.width = box.width;
-	canvas.height = box.height;
-	grd = ctx.createRadialGradient(canvas.width / 2,
-                                   canvas.height / 2,
-                                   0,
-                                   canvas.width / 2,
-                                   canvas.height / 2,
-                                   canvas.height);
+	canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    
+    const centerX = canvas.width / 2;
+    const centerY = canvas.height / 2;
+    
+    grd = ctx.createRadialGradient(
+        centerX, centerY, 0,
+        centerX, centerY, canvas.height
+    );
 }
 
 function draw() {
 	ctx.clearRect(0 ,0 ,canvas.width ,canvas.height);
 	drawBackground();
-
-	for (var i = letters.length -1; i >=0 ; i--) {
-		letters[i].percent +=1 ;
-		letters[i].draw();
-		if(letters[i].percent >100){
-			letters.splice(i ,1 );
-		}
-	}
-	for (var i=0 ;i<keys_down.length;i++){
-		if(keys_down[i]){
-			letters.push(new Letter(i));
-		}
-	}
+    
+    for (var i = letters.length - 1; i >= 0; i--) {
+        letters[i].percent += 1;
+        
+        if (letters[i].percent > 100) {
+            returnLetterToPool(letters[i]);
+            letters.splice(i, 1);
+        } else {
+            letters[i].draw();
+        }
+    }
+	
 	requestAnimationFrame(draw);
 }
-var start_keys =[81 ,87 ,69 ,82 ,84 ,89 ,85];
+
+var start_keys =['F' ,'U' ,'C' ,'K'];
 
 function startAnimation(){
 	setTimeout(function(){
-		var key=start_keys.pop();
-		keys_down[key]=true;
-		setTimeout(function(){
-			keys_down[key]=false;
-		},180 );
+		var key= start_keys.pop();
+        onType(key);
 		if(start_keys.length>0){
 			startAnimation();
 		}
@@ -147,18 +155,35 @@ startAnimation();
 window.onresize=resize;
 
 const inputs = document.querySelectorAll('.text-input');
+const iteration = 3; 
+const rate = 15;
 
 inputs.forEach(input => {
-    input.onkeyup = function (event) {
-        keys_down[event.keyCode] = false;
-    };
-
-    input.onkeydown = function (event) {
-        if (event.keyCode >= 65 && event.keyCode <= 90 || event.keyCode >= 48 && event.keyCode <= 57) {
-            keys_down[event.keyCode] = true;
+    let previousLength = input.value.length;
+    input.addEventListener('input', function(evt) {
+        const currentLength = this.value.length;
+        if (currentLength > previousLength) {
+            const lastChar = this.value.slice(-1);
+            onType(lastChar); 
         }
-    };
+        previousLength = currentLength;
+    });
 });
+
+function onType(char){
+    if (char) {
+        let count = 0; 
+
+        const intervalId = setInterval(() => {
+            if (count < iteration) {
+                letters.push(getLetterFromPool(char));
+                count++;
+            } else {
+                clearInterval(intervalId); 
+            }
+        }, rate); 
+    }
+}
 
 window.requestAnimationFrame=(function(){
 	return window.requestAnimationFrame||
